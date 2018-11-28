7634deae9e14553c87e9294fc9b3dc220d90311f
akourtis <akourtisdev@gmail.com>
2018-11-28 20:48:13 +0100

    project structure generated with by https://start.spring.io/

---
+++ src/main/java/io/akourtis/ptool/PtoolApplication.java
@@ -0,0 +1,12 @@
+package io.akourtis.ptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtoolApplication.class, args);
+	}
+}
---
+++ src/test/java/io/akourtis/ptool/PtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package io.akourtis.ptool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PtoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
